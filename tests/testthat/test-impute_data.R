78786aa7b18b632f7b48040abc397ec03eea375c
rafa6174 <rafles091@gmail.com>
2024-06-06 10:54:28 -0600

    tests

--- tests/testthat/test-impute_data.R
+++ tests/testthat/test-impute_data.R
@@ -1,8 +1,9 @@
-data<-as.dataframe(data)
-tipdat<-c()
-for(i in 1:length(data24)){
-  tipdat<-c(tipdat,is.numeric(data[,i]))
-}
+x<-data24
+x[,8]<-as.character(x[,8])
+sizeTest<-dim(data24)
+testnas<-impute_data(data24,useParallel = TRUE)
 test_that("data type", {
-  expect_identical(tipdat,rep(TRUE,length(data24)))
-})
+  expect_error(impute_data(x,useParallel = TRUE),"numeric")
+  expect_identical(dim(testnas),sizeTest)
+  expect_false(is.na(testnas))
+  })
